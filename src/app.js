3594a239c0a8d2f37da79e9e34d3aff3dc785785
eduardo.haas <eduardo.haas.gomes@escola.pr.gov.br>
2023-06-23 21:48:09 -0300

    Revert "carrosel de imagens"
    This reverts commit 181a38496b0be10e496d8a27cbc69c00f1b50a7d.

--- src/app.js
+++ src/app.js
@@ -3,35 +3,22 @@ import Header from "./components/Header";
 import Banner from "./components/Banner";
 import ProductCard from "./components/ProductCard";
 import Footer from "./components/Footer";
-import Carousel from './components/Carousel';
+import About from './components/about';
 import "./css/style.css";
-import './css/Carousel.css';
 
 const App = () => {
-  const images = [
-    './img/monitor.jpg',
-    './img/processadorintel.png',
-    './img/teclado e mouse.jpg',
-    './img/celular.png'
-  ];
-
   return (
-
+    
     <div className="container">
       <Header />
       <Banner />
       <main className="container">
-      
         <section className="products-container">
-          
-      <Carousel images={images} />
-      
           <ProductCard />
         </section>
       </main>
       <Footer />
     </div>
-
   );
 };
 
