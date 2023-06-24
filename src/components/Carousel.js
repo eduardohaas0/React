6c8dc26ed537a1717e825ab2fd1a649882bb5831
eduardo.haas <eduardo.haas.gomes@escola.pr.gov.br>
2023-06-23 21:51:18 -0300

    Revert "Revert "carrosel de imagens""
    This reverts commit 3594a239c0a8d2f37da79e9e34d3aff3dc785785.

---
+++ src/components/Carousel.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const Carousel = ({ images }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToNextSlide = () => {
+    const nextIndex = (currentIndex + 1) % images.length;
+    setCurrentIndex(nextIndex);
+  };
+
+  const goToPreviousSlide = () => {
+    const prevIndex = (currentIndex - 1 + images.length) % images.length;
+    setCurrentIndex(prevIndex);
+  };
+
+  return (
+    <div className="carousel">
+      <button className="carousel__button carousel__button--prev" onClick={goToPreviousSlide}>
+        voltar
+      </button>
+      <img className="carousel__image" src={images[currentIndex]} alt="Carousel Slide" />
+      <button className="carousel__button carousel__button--next" onClick={goToNextSlide}>
+        proxima
+      </button>
+    </div>
+  );
+};
+
+export default Carousel;
